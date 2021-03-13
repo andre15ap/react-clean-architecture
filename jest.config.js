c5d782b0e5a4f494ab6e60b7ae383eadf415bd88
andre.pinheiro <andre.pinheiro@digitro.com>
2021-03-13 15:27:16 -0300

    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectDirectory: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
